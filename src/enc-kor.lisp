7bcb3839344c14d1c62987945b3f5c93c2e743fe
jonghyouk yun <ageldama@gmail.com>
2014-10-03 21:29:45 +0900

    cp949 octet-counter done.

--- src/enc-kor.lisp
+++ src/enc-kor.lisp
@@ -11,10 +11,15 @@
 (defvar *ucs-to-cp949-hash* (make-hash-table))
 (dolist (p +cp949->unicode+)
   (let ((cp949 (car p))
-        (ucs (cdr p)))
+        (ucs (cadr p)))
     (setf (gethash cp949 *cp949-to-ucs-hash*) ucs)
     (setf (gethash ucs *ucs-to-cp949-hash*) cp949)))
 
+(defun ucs->cp949 (code)
+  (values (gethash code *ucs-to-cp949-hash*)))
+
+(defun cp949->ucs (code)
+  (values (gethash code *cp949-to-ucs-hash*)))
 
 (define-character-encoding :cp949
     "UHC, (CP949, or extended EUC-KR), 1 -or- 2-bytes widely used in Korean."
@@ -25,7 +30,17 @@
 (define-octet-counter :cp949 (getter type)
   `(named-lambda cp949-octet-counter (seq start end max)
      (declare (type ,type seq) (fixnum start end max))
-     (error :NIY-cp949-octet-counter)))
+     (loop with noctets fixnum = 0
+        for i fixnum from start below end
+        for code of-type code-point = (,getter seq i)
+        do
+          (let ((new (cond ((not (null (ucs->cp949 code))) 2)
+                           (t 1))))                
+            (if (and (plusp max) (> new max))
+                (loop-finish)
+                (setq noctets new)))
+        finally (return (values noctets i)))))
+     
 
 ; for code of-type code-point = (,getter seq i)
 ;           for i fixnum from start below end
