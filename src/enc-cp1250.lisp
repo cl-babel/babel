3f93a35ccbe88c897bfbce232d9fa3a0873f4967
Bartosz Knapik <knapikbartek@gmail.com>
2025-08-25 10:14:37 +0200

    Fix load failure cp1250

--- src/enc-cp1250.lisp
+++ src/enc-cp1250.lisp
@@ -58,7 +58,8 @@
       for unicode across +cp1250-to-unicode+
       unless (= unicode #xfffd)
         do (setf (gethash unicode h) code)
-      finally (return h)))
+      finally (return h))
+  :test #'equalp)
 
 (define-unibyte-encoder :cp1250 (code)
   (cond ((< code #x80) code)
